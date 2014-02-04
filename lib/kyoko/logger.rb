ed1121c180247a336d8038f2a97ce9a1fdb3ebb8
remain <ponde.ponde.ponde@gmail.com>
2014-02-04 23:50:22 +0900

    change to singleton class

--- lib/kyoko/logger.rb
+++ lib/kyoko/logger.rb
@@ -1,15 +1,17 @@
-require "logger"
 require "forwardable"
+require "logger"
+require "singleton"
 
 class Kyoko
   class Logger
-    extend Forwardable
+    include Singleton
+    extend  Forwardable
 
     def_delegators :@logger, :debug, :info, :warn, :error, :fatal
 
-    def initialize(logdev: STDOUT, level: ::Logger::INFO)
-      @logger = ::Logger.new(logdev)
-      @logger.level = level
+    def initialize
+      @logger = ::Logger.new(STDOUT)
+      @logger.level = ::Logger::INFO
       @logger.formatter = proc do |severity, datetime, progname, msg|
         "[#{datetime.strftime('%Y-%m-%d %H:%M:%S')}] #{severity}: #{msg}\n"
       end
